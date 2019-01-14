89e7dd1cae3d55a96d86d72d786014b750959505
moaiad123 <45014491+moaiad123@users.noreply.github.com>
2019-01-14 10:50:00 -0800

    Add files via upload

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 let channel =     client.channels.get("519144528146464768")
 setInterval(function() {
 channel.send(`spamfarmcreaditlol`);
-}, 30)
+}, 100)
 })
 
 client.login(process.env.BOT_TOKEN);
